d1278ea14b07e9b98729136592f380a3afaa0209
j.a.s <aras.strong8@gmail.com>
2023-12-09 12:20:14 -0300

    api de consulta de vacina

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 
 const paciente = require("./src/routes/paciente");
+const consultaVacina = require("./src/routes/consulta_vacina");
 const PORT = 3000; // porta do app
 
 //midleware
 app.use(express.json());
 // Defina rotas e lógica de manipulação aqui
 app.use(paciente);
+app.use(consultaVacina);
 
 app.listen(PORT, () => {
   console.log(`Servidor Express iniciado em http://localhost:${PORT}`);
