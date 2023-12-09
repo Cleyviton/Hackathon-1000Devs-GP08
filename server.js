ee0a80ce36bb8b464322396debf33bd5ad12e5d5
Cleyviton <cleyvitonkeke@gmail.com>
2023-12-09 12:56:02 -0300

    feat(vacinaAplicada): criando rota de buscar vacinas aplicadas

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const express = require("express");
 const app = express();
 
 const paciente = require("./src/routes/paciente");
+const vacinaAplicada = require("./src/routes/vacinaAplicada");
 const PORT = 3000; // porta do app
 
 //midleware
 app.use(express.json());
 // Defina rotas e lógica de manipulação aqui
 app.use(paciente);
+app.use(vacinaAplicada);
 
 app.listen(PORT, () => {
-  console.log(`Servidor Express iniciado em http://localhost:${PORT}`);
+    console.log(`Servidor Express iniciado em http://localhost:${PORT}`);
 });
