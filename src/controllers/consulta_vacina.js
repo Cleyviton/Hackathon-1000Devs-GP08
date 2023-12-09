912d39d071ae1d554e65680787931dccd3bbf8ff
j.a.s <aras.strong8@gmail.com>
2023-12-09 15:38:00 -0300

    api para consultar vacinas por idade

--- src/controllers/consulta_vacina.js
+++ src/controllers/consulta_vacina.js
@@ -17,12 +17,58 @@ module.exports = {
   async getConsultaVacinaPorIdade(req, res, next) {
     try {
       const { idade } = req.params;
+      console;
       const result = await pool.query(
-        "SELECT * FROM PACIENTE WHERE EXTRACT(YEAR FROM AGE(NOW(), Data_nascimento)) = $1",
+        "SELECT EXTRACT(YEAR FROM AGE(NOW(), Data_nascimento)) as idade,* " +
+          "FROM PACIENTE INNER JOIN VACINAAPLICADA va ON PACIENTE.id_paciente = va.id_paciente " +
+          "INNER JOIN VACINA ON VACINA.Id_vacina = va.Id_vacina " +
+          "WHERE EXTRACT(YEAR FROM AGE(NOW(), Data_nascimento)) = $1 ",
+
         [idade]
       );
 
-      return res.json(result.rows);
+      const groupedByPatientId = result.rows.reduce((acc, row) => {
+        const existingPatient = acc.find(
+          (patient) => patient.id_paciente === row.id_paciente
+        );
+
+        if (existingPatient) {
+          // Paciente já existe, adiciona a vacina ao array de vacinas
+          existingPatient.vacinas.push({
+            id_vacina: row.id_vacina,
+            data_aplicacao: row.data_aplicacao,
+            vacina: row.vacina,
+            sigla_vacina: row.sigla_vacina,
+            doenca_protecao: row.doenca_protecao,
+            dose: row.dose,
+            id_rede: row.id_rede,
+          });
+        } else {
+          // Paciente não existe, cria um novo paciente
+          const newPatient = {
+            id_paciente: row.id_paciente,
+            nome: row.nome,
+            data_nascimento: row.data_nascimento,
+            idade: row.idade,
+            vacinas: [
+              {
+                id_vacina: row.id_vacina,
+                data_aplicacao: row.data_aplicacao,
+                vacina: row.vacina,
+                sigla_vacina: row.sigla_vacina,
+                doenca_protecao: row.doenca_protecao,
+                dose: row.dose,
+                id_rede: row.id_rede,
+              },
+            ],
+          };
+
+          acc.push(newPatient);
+        }
+
+        return acc;
+      }, []);
+      return res.json(groupedByPatientId);
     } catch (error) {
       console.log(error);
       return res.json(error);
