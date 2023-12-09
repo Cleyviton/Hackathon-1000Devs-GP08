61cd7b3b3f62ea6d139dea98166cf69ef9ffbf9f
j.a.s <aras.strong8@gmail.com>
2023-12-09 10:08:40 -0300

    select table

--- src/controllers/paciente.js
+++ src/controllers/paciente.js
@@ -3,7 +3,7 @@ const pool = require("../../database");
 module.exports = {
   async getPaciente(req, res, next) {
     try {
-      const result = await pool.query("SELECT * from VACINA");
+      const result = await pool.query("SELECT * from PACIENTE");
       return res.json(result.rows);
     } catch (error) {
       return res.json(error);
