f74bce067e50855039cccf2a372e7f80e4881520
Cleyviton <cleyvitonkeke@gmail.com>
2023-12-10 01:42:18 -0300

    feat(getVacinasPendentes): criando rota de buscar vacinas pendentes de um paciente

--- src/controllers/consulta_vacina.js
+++ src/controllers/consulta_vacina.js
@@ -106,7 +106,43 @@ module.exports = {
     async getConsultaVacinaPendentePorPaciente(req, res, next) {
         try {
             const { id_paciente } = req.params;
+
+            const paciente = await pool.query(
+                `SELECT * FROM PACIENTE WHERE Id_paciente = $1`,
+                [id_paciente]
+            );
+
+            if (paciente.rowCount <= 0) {
+                return res.status(404).json({
+                    msg: "Paciente não encontrado",
+                });
+            }
+
+            const dataDeNascimento = new Date(paciente.rows[0].data_nascimento);
+            const dataAtual = new Date();
+            const idade =
+                dataAtual.getFullYear() - dataDeNascimento.getFullYear();
+
+            const vacinasAplicadas = await pool.query(
+                `SELECT V.* from vacina v
+                join vacinaaplicada va on va.id_vacina = v.id_vacina 
+                join paciente p on p.id_paciente = va.id_paciente
+                where va.id_paciente = $1`,
+                [paciente.rows[0].id_paciente]
+            );
+
+            const vacinasPossiveis = await pool.query(
+                `SELECT V.*, PA.desc_ano FROM VACINA V JOIN PERIODOAPLICACAOANO PA ON V.Id_vacina = PA.Id_vacina WHERE $1 BETWEEN PA.Qtd_ano_inicial AND PA.Qtd_ano_final`,
+                [idade]
+            );
+
+            const vacinasPendentes = vacinasPossiveis.rows.filter(
+                (element) => !vacinasAplicadas.rows.includes(element)
+            );
+
+            return res.json(vacinasPendentes);
         } catch (error) {
+            console.log(error);
             return res.json(error);
         }
     },
