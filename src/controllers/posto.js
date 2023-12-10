d53923c59f762734294a1157267d08f8a446def4
Cleyviton <cleyvitonkeke@gmail.com>
2023-12-10 11:53:07 -0300

    hot(createPosto): corrigindo a crianção de posto

--- src/controllers/posto.js
+++ src/controllers/posto.js
@@ -1,30 +1,33 @@
 const pool = require("../../database");
 
 module.exports = {
-  async createPosto(req, res, next) {
-    try {
-      const { endereco, id_rede, nome } = req.query;
-      const result = await pool.query(
-        `INSERT INTO POSTO (endereco, id_rede,nome) VALUES ($1, $2, $3) RETURNING id`,
-        [endereco, id_rede, nome]
-      );
-      return res.json(result.rows);
-    } catch (error) {
-      return res.json(error);
-    }
-  },
+    async createPosto(req, res, next) {
+        try {
+            const { endereco, id_rede, nome } = req.body;
+            const result = await pool.query(
+                `INSERT INTO POSTO (nome, endereco, id_rede) 
+                VALUES ($1, $2, $3)
+                RETURNING *;`,
+                [nome, endereco, id_rede]
+            );
+            return res.json(result.rows);
+        } catch (error) {
+            console.log(error);
+            return res.json(error);
+        }
+    },
 
-  async getEnderecoPosto(req, res, next) {
-    try {
-      const { endereco } = req.query;
-      const result = await pool.query(
-        `SELECT * FROM POSTO WHERE endereco ILIKE $1`,
-        [`%${endereco}%`]
-      );
-      return res.json(result.rows);
-    } catch (error) {
-      console.log(error);
-      return res.json(error);
-    }
-  },
+    async getEnderecoPosto(req, res, next) {
+        try {
+            const { endereco } = req.query;
+            const result = await pool.query(
+                `SELECT * FROM POSTO WHERE endereco ILIKE $1`,
+                [`%${endereco}%`]
+            );
+            return res.json(result.rows);
+        } catch (error) {
+            console.log(error);
+            return res.json(error);
+        }
+    },
 };
--- src/routes/posto.js
+++ src/routes/posto.js
@@ -2,50 +2,39 @@ const express = require("express");
 
 const postoController = require("../controllers/posto");
 const routes = express.Router();
+
 /**
  * @swagger
  * /posto/create:
  *   post:
- *     summary: Cria um novo posto.
+ *     summary: Cria um novo posto
  *     tags:
  *       - Posto
  *     description: Endpoint para criar um novo posto.
- *     parameters:
- *       - in: query
- *         name: endereco
- *         description: Endereço do posto.
- *         required: true
- *         type: string
- *       - in: query
- *         name: id_rede
- *         description: ID da rede associada ao posto.
- *         required: true
- *         type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               endereco:
+ *                 type: string
+ *               id_rede:
+ *                 type: integer
+ *               nome:
+ *                 type: string
  *     responses:
  *       200:
- *         description: novo posto criado com sucesso.
- *         content:
- *           application/json:
- *             example:
- *               - id: 1
- *       400:
- *         description: Erro na requisição.
- *         content:
- *           application/json:
- *             example:
- *               - error: Mensagem de erro específica.
- *       500:
- *         description: Erro interno do servidor
+ *         description: Posto criado com sucesso
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 mensagem:
- *                   type: string
- *                   description: Mensagem de erro
- *             example:
- *               mensagem: "Erro interno do servidor"
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   // Defina as propriedades de retorno aqui
  */
 routes.post("/posto/create", postoController.createPosto);
 
